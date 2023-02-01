c44f4e54d80c9a7635100d9bf1c2a64db2d590d5
Hunter-Mayer <hmayer95@gmail.com>
2023-02-01 17:01:21 -0600

    About Component

--- src/components/About.js
+++ src/components/About.js
@@ -6,14 +6,12 @@ export default function About() {
       <div className="container mx-auto flex px-10 py-20 md:flex-row flex-col items-center">
         <div className="lg:flex-grow md:w-1/2 lg:pr-24 md:pr-16 flex flex-col md:items-start md:text-left mb-16 md:mb-0 items-center text-center">
           <h1 className="title-font sm:text-4xl text-3xl mb-4 font-medium text-white">
-            Hi, I'm Reed.
+            Hi, I'm Hunter.
             <br className="hidden lg:inline-block" />I love to build amazing
             apps.
           </h1>
           <p className="mb-8 leading-relaxed">
-            Lorem ipsum dolor sit amet, consectetur adipisicing elit. Qui
-            laborum quasi, incidunt dolore iste nostrum cupiditate voluptas?
-            Laborum, voluptas natus?
+          I am an aspiring software developer who is passionate about using technology to help other people and create cool projects. I recently graduated from the University of Wisconsin Web Development Bootcamp, where I learned the Agile software development model by working with my peers to create various applications. In my spare time I enjoy spending time with my fiancée and four cats. 
           </p>
           <div className="flex justify-center">
             <a
@@ -31,8 +29,8 @@ export default function About() {
         <div className="lg:max-w-lg lg:w-full md:w-1/2 w-5/6">
           <img
             className="object-cover object-center rounded"
-            alt="hero"
-            src="./coding.svg"
+            alt="Image of Hunter Mayer"
+            src="./Hunter_Mayer_Professional.jpg"
           />
         </div>
       </div>
